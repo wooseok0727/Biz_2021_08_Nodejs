c71572662d1d31e8f71d829d00091736d3d9f01a
wooseok0727 <crash2@naver.com>
2021-08-23 14:59:34 +0900

    8월 23일

---
+++ nodejs_004/database/bbs.sql
@@ -0,0 +1,12 @@
+-- % : 어디세어나 접근가능
+CREATE USER 'node'@'%'
+IDENTIFIED BY '12345';
+
+-- 모든 권한 부여
+GRANT ALL PRIVILEGES ON *.*
+TO 'node'@'%';
+
+CREATE DATABASE nodeDB;
+USE nodeDB;
+DESC tbl_bbs;
+DROP TABLE tbl_bbs;
